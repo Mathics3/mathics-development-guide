397f3f6dd23e25daa8596078d95bdf0c024c45cc
Pablo Emilio Escobar Gaviria <pablo-escobar@riseup.net>
2021-01-09 08:54:22 +1000

    Started working on the script to convert the tables to RST
    Also fixed multiple errors in named-characters-data.csv

---
+++ admin-tools/generate-tables.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import csv
+
+def string_from_codes(codes):
+    """Generate a unicode string from a list of code points"""
+    return "".join(chr(int(c[2:], 16)) for c in codes.split(" ") if c)
+
+def unicode_url_from_code(code):
+    return "https://www.compart.com/en/unicode/U+" + f"{int(wl_code[2:], 16):X}".zfill(4)
+
+with open("resources/named-characters-data.csv", "r") as f:
+    reader = csv.reader(f, delimiter=",", quotechar="|")
+    next(reader) # Skip the header
+
+    # The rows formatted in RST
+    rst_rows = []
+
+    for row in reader:
+        named, uni_name, wl_name, uni_code, wl_code, esc = row
+
+        uni_code, wl_code = uni_code.strip(), wl_code.strip()
+        print(named)
+        esc = "".join("\\" + c for c in esc.replace(" ", "␣"))
+
+        named_url = f"https://reference.wolfram.com/language/ref/{named[2:-1]}.html"
+        named = named.replace("\\", "\\\\")
+
+        if uni_code:
+            uni_code_rst = " ".join(f"`{c} <{unicode_url_from_code(c)}>`_" 
+                                    for c in uni_code.split(" ")) 
+            uni_code_rst += " \\" + string_from_codes(uni_code)
+        
+        parts = [f"   * - `{named} <{named_url}>`_", uni_name, wl_name, 
+                uni_code_rst if uni_code else "",
+                f"`{wl_code} <{unicode_url_from_code(wl_code)}>`_", 
+                f"``{esc}``" if esc else "",]
+        rst_row = "\n     - ".join(parts)
+        rst_rows.append(rst_row)
+
+    print("\n".join(rst_rows))
+
+
+
